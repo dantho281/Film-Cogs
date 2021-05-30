f7c2b35d06a4f82135c967c434e2ba0040932216
Daniel T. Thorpe <daniel.thorpe@citrix.com>
2021-05-30 21:07:00 +0100

    feat(contests.py): add message listener

--- contests/contests.py
+++ contests/contests.py
@@ -15,9 +15,67 @@ class ContestsCog(commands.Cog):
 
         default_guild_config = {
             "posting_channel": None,
+            "listen_channel": None
         }
 
         self.config.register_guild(**default_guild_config)
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.message.author.bot or not ctx.message.guild:
+            return
+        if ctx.message.channel.id is not await self.config.guild(ctx.guild).listen_channel:
+            return
+        async with ctx.channel.typing():
+            channel_id = await self.config.guild(ctx.guild).posting_channel()
+            channel = ctx.guild.get_channel(channel_id)
+            error_channel = ctx.guild.get_channel(ctx.message.channel.id)
+            if len(ctx.message.attachments) > 0:
+                tempfile = await ctx.message.attachments[0].read()
+                mimetype = ctx.message.attachments[0].content_type
+                if "image/" in mimetype:
+                    extension = mimetypes.guess_extension(mimetype)
+                    author = ctx.message.author.name
+                    author_id = ctx.message.author.id
+                    try:
+                        await ctx.message.delete()
+                    except:
+                        await error_channel.send(
+                            content="Unable to delete submission request automatically, please delete your post yourself.",
+                            delete_after=60,
+                            reference=ctx.message,
+                            mention_author=True
+                        )
+                    filehash = hashlib.md5(tempfile)
+                    filename = filehash.hexdigest()
+                    contests_database_temp = await self.config.guild(ctx.guild).contests_database()
+                    if type(contests_database_temp) is not dict:
+                        contests_database_temp = {}
+                    if contests_database_temp[filename] is None:
+                        complete_name = f"{filename}{extension}"
+                        discordfile = discord.File(filename=complete_name, fp=(io.BytesIO(tempfile)))
+                        await channel.send(content=filename, file=discordfile)
+                        contests_database_temp[filename] = {
+                            "author": author,
+                            "author_id": author_id,
+                        }
+                        await self.config.guild(ctx.guild).contests_database.set(contests_database_temp)
+                    else:
+                        await error_channel.send(
+                            content=f"<@{author_id}> this image has been submitted previously, duplicate submissions are not allowed."
+                        )
+                else:
+                    await error_channel.send(
+                        content="Please upload an image, not another type of file.",
+                        reference=ctx.message,
+                        mention_author=True
+                    )
+            else:
+                await error_channel.send(
+                    content="Submission failed. Please attach an image to your message.",
+                    reference=ctx.message,
+                    mention_author=True
+                )
 
     @commands.group(name="contest")
     async def _contests(self, ctx):
@@ -25,18 +83,34 @@ class ContestsCog(commands.Cog):
 
     @commands.guild_only()
     @checks.mod()
-    @_contests.command(name="setchannel")
+    @_contests.command(name="setpostchannel")
     async def set_posting_channel(self, ctx, channel: discord.TextChannel):
         """Set the submission posting channel for this server.
 
         Usage:
-        - `[p]contests setchannel <channel>`
+        - `[p]contests setpostchannel <channel>`
         """
         await self.config.guild(ctx.guild).posting_channel.set(channel.id)
         check_value = await self.config.guild(ctx.guild).posting_channel()
         success_embed = discord.Embed(
             title="Submissions channel set!",
             description=f"Submissions channel set to <#{check_value}>",
+            colour=await ctx.embed_colour()
+        )
+        await ctx.send(embed=success_embed)
+
+    @_contests.command(name="setlistenchannel")
+    async def set_posting_channel(self, ctx, channel: discord.TextChannel):
+        """Set the submission listening channel for this server.
+
+        Usage:
+        - `[p]contests setlistenchannel <channel>`
+        """
+        await self.config.guild(ctx.guild).posting_channel.set(channel.id)
+        check_value = await self.config.guild(ctx.guild).listening_channel()
+        success_embed = discord.Embed(
+            title="Listening channel set!",
+            description=f"Listening channel set to <#{check_value}>",
             colour=await ctx.embed_colour()
         )
         await ctx.send(embed=success_embed)
